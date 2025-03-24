1a15f2e17a4f104ee0c4bb2fccff3a51f9cfe3a1
Arnaud Henric <arnaud.henric@gmail.com>
2025-03-24 12:25:12 +0100

    Add main guard

--- blheli.py
+++ blheli.py
@@ -7,87 +7,89 @@ import coloredlogs
 from blheli_silabs import BlHeliSilabs
 from blheli_atmel import BlHeliAtmel
 
-def parse_key_value_pair(pair):
-    """Parses key=value strings into (key, value) tuples."""
-    key, value = pair.split("=", 1)
-    return key, value
+if __name__ == '__main__':
 
-# Create the parser
-parser = argparse.ArgumentParser(description="Python BLHeli command-line configurator")
+    def parse_key_value_pair(pair):
+        """Parses key=value strings into (key, value) tuples."""
+        key, value = pair.split("=", 1)
+        return key, value
 
-parser.add_argument("port", help="Serial port")
-parser.add_argument("--baudrate", type=int, default=115200, help="Serial baudrate")
-parser.add_argument("--count", type=int, default=4, help="Number of ESC instances, default is 4")
-parser.add_argument("--interface", choices=['atmel', 'silabs'], default='silabs', help="Choose the chip type: 'atmel' or 'silabs'")
-parser.add_argument("--verbose", "-v", action="store_true", help="Increase output verbosity")
-parser.add_argument("--json", "-j", action="store_true", help="Output data in JSON format to ease integration")
+    # Create the parser
+    parser = argparse.ArgumentParser(description="Python BLHeli command-line configurator")
 
-# Create subparsers
-subparsers = parser.add_subparsers(dest='command')
+    parser.add_argument("port", help="Serial port")
+    parser.add_argument("--baudrate", type=int, default=115200, help="Serial baudrate")
+    parser.add_argument("--count", type=int, default=4, help="Number of ESC instances, default is 4")
+    parser.add_argument("--interface", choices=['atmel', 'silabs'], default='silabs', help="Choose the chip type: 'atmel' or 'silabs'")
+    parser.add_argument("--verbose", "-v", action="store_true", help="Increase output verbosity")
+    parser.add_argument("--json", "-j", action="store_true", help="Output data in JSON format to ease integration")
 
-# Create get config command
-get_config = subparsers.add_parser('get_config', help='Read and display configuration')
-get_config.add_argument("--esc", type=int, default=0, help="ESC instance, default is all")
+    # Create subparsers
+    subparsers = parser.add_subparsers(dest='command')
 
-# Create set_config command
-set_config = subparsers.add_parser('set_config', help='Write configuraiton parameters')
-set_config.add_argument("--esc", type=int, default=0, help="ESC instance, default is all")
-set_config.add_argument("--params", nargs="+", action="append", type=parse_key_value_pair, help="Parameters list in the form key=value", required=True)
+    # Create get config command
+    get_config = subparsers.add_parser('get_config', help='Read and display configuration')
+    get_config.add_argument("--esc", type=int, default=0, help="ESC instance, default is all")
 
-# Parse the arguments
-args = parser.parse_args()
+    # Create set_config command
+    set_config = subparsers.add_parser('set_config', help='Write configuraiton parameters')
+    set_config.add_argument("--esc", type=int, default=0, help="ESC instance, default is all")
+    set_config.add_argument("--params", nargs="+", action="append", type=parse_key_value_pair, help="Parameters list in the form key=value", required=True)
 
-coloredlogs.install(level=logging.INFO, fmt='%(asctime)s %(levelname)-8s %(name)-10s %(message)s')
-logger = logging.getLogger('blheli')
+    # Parse the arguments
+    args = parser.parse_args()
 
-# Setup log level
-if args.verbose:
-    coloredlogs.set_level('DEBUG')
+    coloredlogs.install(level=logging.INFO, fmt='%(asctime)s %(levelname)-8s %(name)-10s %(message)s')
+    logger = logging.getLogger('blheli')
 
-# Instanciate interface and connect to ESC
-if args.interface == 'silabs':
-    interface = BlHeliSilabs(args.port, args.baudrate, args.count)
-else:
-    interface = BlHeliAtmel(args.port, args.baudrate, args.count)
+    # Setup log level
+    if args.verbose:
+        coloredlogs.set_level('DEBUG')
 
-try:
+    # Instanciate interface and connect to ESC
+    if args.interface == 'silabs':
+        interface = BlHeliSilabs(args.port, args.baudrate, args.count)
+    else:
+        interface = BlHeliAtmel(args.port, args.baudrate, args.count)
 
-    # Connect and test connection
-    interface.connect()
+    try:
 
-    if args.command == 'get_config':
+        # Connect and test connection
+        interface.connect()
 
-        if args.esc == 0:
-            data = interface.read_config_all()
-        else:
-            data = interface.read_config(args.esc)
+        if args.command == 'get_config':
 
-    elif args.command == 'set_config':
+            if args.esc == 0:
+                data = interface.read_config_all()
+            else:
+                data = interface.read_config(args.esc)
 
-        # Flatten the list of lists into a dictionary
-        params = {key: value for pair in args.params for key, value in pair}
-        if args.esc == 0:
-            interface.write_config_all(**params)
-            data = None
-        else:
-            interface.write_config(args.esc, **params)
-            data = None
+        elif args.command == 'set_config':
 
-    status = 'success'
+            # Flatten the list of lists into a dictionary
+            params = {key: value for pair in args.params for key, value in pair}
+            if args.esc == 0:
+                interface.write_config_all(**params)
+                data = None
+            else:
+                interface.write_config(args.esc, **params)
+                data = None
 
-except Exception as e:
+        status = 'success'
 
-    data = str(e)
-    status = 'error'
+    except Exception as e:
 
-finally:
+        data = str(e)
+        status = 'error'
 
-    interface.disconnect()
+    finally:
+
+        interface.disconnect()
 
 
-if args.json:
-    print(json.dumps({'status': status, 'data': data}))
-else:
-    logger.info(f'Status: {status}')
-    if data:
-        print(data)
+    if args.json:
+        print(json.dumps({'status': status, 'data': data}))
+    else:
+        logger.info(f'Status: {status}')
+        if data:
+            print(data)
